2626d64cb448d5835d849e5e5052d0e3e02941b5
loowodi <igos2006@gmail.com>
2024-08-20 23:45:19 +1000

    first commit

---
+++ js/WalletButton.js
@@ -0,0 +1,4 @@
+const WalletButton=()=>{const{ethereum}=window;const[showModal,setShowModal]=React.useState(false);const[addr,setAddr]=React.useState("");if(ethereum){ethereum.on("accountsChanged",(a)=>setAddr(a.length?a[0]:""));ethereum.on("connect",(a)=>setAddr(a.length?a[0]:""));}
+React.useEffect(()=>{if(ethereum){ethereum.request({method:"eth_accounts",}).then((a)=>setAddr(a[0]));}},[]);return(<><ConnectWalletButton account={addr}/></>);};const ConnectWalletButton=(props)=>{const{account}=props;return(<><button
+onClick={async()=>{connect("mm");}}
+className="wallet-button">{account?`${account.slice(0,5)}...${account.slice(-5)}`:"Connect Wallet"}</button></>);};
---
+++ js/showMess.js
@@ -0,0 +1,3 @@
+function showSuccess(text){alert(text);}
+function showError(text){alert(text);}
+function showInfo(text){alert(text);}
